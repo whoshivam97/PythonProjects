3ba49879d2fad8556298e547d0a04910d685ec0e
whoshivam97 <whoshivam97@gmail.com>
2023-03-04 19:08:24 +0530

    Updated Code

---
+++ Section2(While-loops,Methods)/App1/main.py
@@ -0,0 +1,8 @@
+# While Basic Example
+user_prompt = "Enter the To-Do:"
+while True:
+    todo = input(user_prompt)
+    print("todo")
+    print("Next...")
+
+
---
+++ Section2(While-loops,Methods)/App1/methodusecase.py
@@ -0,0 +1,8 @@
+user_prompt = "Enter a To-Do:"
+
+todos = []
+
+while True:
+    todo = input(user_prompt)
+    todos.append(todo)
+    print(todos)
