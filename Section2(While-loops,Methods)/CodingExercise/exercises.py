a81c7b55f40d0ccccedcbdc2f99726a6c042fd53
whoshivam97 <whoshivam97@gmail.com>
2023-03-05 01:30:42 +0530

    Updated Code

---
+++ Section2(While-loops,Methods)/CodingExercise/exercises.py
@@ -0,0 +1,23 @@
+Coding Exercise 1
+Create a program that prompts the user to input their name once.
+Then, the program prints out the name once with the first letter capitalized.
+
+name = input("What is your name? ")
+capitalized_name = name.capitalize()
+print(capitalized_name)
+
+Coding Exercise 2
+Create a program that prompts the user to input their name once.
+Then, the program repeatedly prints out the name with the first letter capitalized.
+
+name = input("what is your name? ")
+while True:
+    print(name.capitalize())
+
+Create a program that prompts the user to input their name repeatedly.
+Then, the program repeatedly prints out the name with the first letter capitalized
+
+while True:
+    name = input("What is your name? ")
+    print(name.capitalize())
+
